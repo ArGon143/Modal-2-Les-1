8a8bd26bd54b919b97cccbf775c06c21bc1b62b4
argon <argon143@mail.ru>
2023-11-12 20:42:49 +0300

    Модуль 2. Урок 1. ДЗ № 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
-// декларативный стиль начало
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div>{new Date().getFullYear()}</div>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('div', null, new Date().getFullYear()),
+		),
 	);
 };
-// декларативный стиль конец
